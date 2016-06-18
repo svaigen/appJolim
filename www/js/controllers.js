20ef919a4a5b0e8f4e20055fca6153481ca5b53e
Alisson Renan <Alisson Renan>
2016-06-18 00:18:12 -0300

    tela inicial completa

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,48 @@
 angular.module('starter.controllers',[])
-  .controller('IndexCtrl',function(){})
+  .controller('IndexCtrl',function($state, $scope){
+    //variaveis da animacao
+    var title = "#indexTitle";
+    var lead = "#indexLead";
+    var rhythms = "#indexRhythms";
+    var logo = "#logo";
+    var imgLogo = "#imgLogo";
+    listRhytms = ["Rock","Samba","Axé","Sertanejo"];
+
+    //definicao da variavel da animação
+    var tl = new TimelineMax({
+      onComplete: function(){ //esta funcao define que ao se completar a timeline, ela deve voltar para o checkpoint denominado "loop"
+        tl.seek("loop");
+      }
+    });
+
+    //definindo o tamanho e posicionando os elementos
+    tl.set(title, {y: window.innerHeight * 0.1, x: (window.innerWidth/2 - document.getElementById('indexTitle').offsetWidth/2)})
+      .set(imgLogo, {width: window.innerWidth * 0.5})
+      .set(logo, {y: window.innerHeight * 0.2, x: (window.innerWidth/2 - document.getElementById('logo').offsetWidth/2)})
+      .set(lead, {y: window.innerHeight * 0.6, x: (window.innerWidth/2 - document.getElementById('indexLead').offsetWidth/2)})
+      .set(rhythms, {y: window.innerHeight * 0.7});
+
+    //deixando os elementos invisiveis
+    tl.set(title, {autoAlpha: 0})
+      .set(logo, {autoAlpha: 0})
+      .set(lead, {autoAlpha: 0})
+      .set(rhythms, {autoAlpha: 0});
+
+    //realizando a animação
+    tl.to(title,5,{autoAlpha:1})
+    .to(logo,5,{autoAlpha:1,delay:-3})
+    .to(lead,2,{autoAlpha:1,delay:-3});
+
+    //ritmos em loop
+    tl.add("loop") //cria um checkpoint na animacao
+    for(let r of listRhytms){
+      tl.set(rhythms, {text: "<i class=\"icon ion-music-note\"></i>  "+ r +"  <i class=\"icon ion-music-note\"></i>", x:0});
+      tl.to(rhythms,2,{autoAlpha:1})
+        .to(rhythms,5,{x: window.innerWidth,delay:-2})
+        .to(rhythms,2,{autoAlpha:0,delay:-2});
+    }
+
+  })
 
   .controller('PreferencesCtrl',function(){})
 
